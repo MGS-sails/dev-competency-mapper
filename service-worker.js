40076ee1e117f9497d68529040703a7aed14c0d1
Mahfouz Shehu <mahfouz@ebi.ac.uk>
2022-02-23 08:47:51 +0000

    latest build 23 feb 2022

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-competency-mapper/precache-manifest.fd3455cacc9854ff66bdc34da4e5b83c.js"
+  "/dev-competency-mapper/precache-manifest.84c90baad728e2d02c0d57c4b7b5e070.js"
 );
 
 self.addEventListener('message', (event) => {
